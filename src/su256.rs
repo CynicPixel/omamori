4c4c4c908a7b8112758650d67d113b2f23b8c477
Suvojit Das <suvojit31202@gmail.com>
2024-11-06 19:11:28 +0530

    Added hex->u256 func for our wrapper of primitive-types

---
+++ src/su256.rs
@@ -0,0 +1,22 @@
+use hex;
+use primitive_types::U256;
+use std::str::FromStr;
+use std::string::ToString;
+#[derive(Debug, Clone)]
+pub struct SU256 {
+    pub v: U256,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+
+pub struct SU256ParseError;
+
+impl FromStr for SU256 {
+    type Err = SU256ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match U256::from_str_radix(s, 16) {
+            Ok(n) => Ok(Self { v: n }),
+            Err(_) => Err(SU256ParseError),
+        }
+    }
+}
